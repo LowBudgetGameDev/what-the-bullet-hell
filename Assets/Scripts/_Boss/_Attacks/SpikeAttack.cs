01cdae489da811ad4e1d6144015296d359095874
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2024-07-05 12:42:31 -0400

    Spike Attack Done

--- Assets/Scripts/_Boss/_Attacks/SpikeAttack.cs
+++ Assets/Scripts/_Boss/_Attacks/SpikeAttack.cs
@@ -7,18 +7,36 @@ public class SpikeAttack : MonoBehaviour, IBossAttack
     private Transform spikes;
     private Transform spikesInstance;
 
+    private new Rigidbody2D rigidbody2D;
+
     private void Awake()
     {
         spikes = Resources.Load<Transform>("Spikes");
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     public void Attack()
     {
         spikesInstance = Instantiate(spikes, transform);
+
+        PhysicsMaterial2D material = new PhysicsMaterial2D();
+        material.bounciness = 1;
+        material.friction = 0;
+        rigidbody2D.sharedMaterial = material;
+
+        float moveSpeed = 25f;
+        rigidbody2D.velocity = UtilsClass.AngleDegreesToVector(Random.Range(0, 360)) * moveSpeed;
+        rigidbody2D.angularVelocity = 120f;
     }
 
     public void StopAttack()
     {
         Destroy(spikesInstance.gameObject);
+
+        rigidbody2D.sharedMaterial = null;
+
+        rigidbody2D.velocity = Vector2.zero;
+        rigidbody2D.angularVelocity = 0f;
+        rigidbody2D.rotation = 0f;
     }
 }
