64ff7f2ced77ad34c5d8dccf9c4f71c859226e9e
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2024-07-08 16:22:40 -0400

    MANY MANY MANY bullets attack done

--- Assets/Scripts/_Boss/_Attacks/ManyBulletsAttack.cs
+++ Assets/Scripts/_Boss/_Attacks/ManyBulletsAttack.cs
@@ -4,13 +4,60 @@ using UnityEngine;
 
 public class ManyBulletsAttack : MonoBehaviour, IBossAttack
 {
+    private Transform bulletPrefab;
+    private new Rigidbody2D rigidbody2D;
+
+    private int numBullets = 20;
+    private float shootTimerMax = 0.25f;
+    private int shootDamage = 1;
+
+    private float shootTimer;
+    private bool isAttacking;
+
+    private void Awake()
+    {
+        bulletPrefab = Resources.Load<Transform>("BossBullet");
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (!isAttacking) return;
+
+        shootTimer -= Time.deltaTime;
+
+        if (shootTimer < 0f)
+        {
+            Shoot();
+            shootTimer += shootTimerMax;
+        }
+    }
+
+    private void Shoot()
+    {
+        for (int i = 0; i < numBullets; i++)
+        {
+            Transform bullet = ObjectPooler.Instance.InstantiateWithPool(bulletPrefab, transform.position, Quaternion.identity);
+
+            float angleDegrees = 360f / numBullets * i;
+            float pointAngleDegrees = UtilsClass.VectorToAngleDegrees(transform.up);
+
+            if (pointAngleDegrees < 0f) pointAngleDegrees += 360f;
+
+            bullet.GetComponent<Bullet>().SetUp(UtilsClass.AngleDegreesToVector(pointAngleDegrees + angleDegrees), shootDamage, transform);
+        }
+    }
+
     public void Attack()
     {
-
+        isAttacking = true;
+        rigidbody2D.angularVelocity = -90f;
     }
 
     public void StopAttack()
     {
-
+        isAttacking = false;
+        rigidbody2D.angularVelocity = 0f;
+        rigidbody2D.rotation = 0f;
     }
 }
