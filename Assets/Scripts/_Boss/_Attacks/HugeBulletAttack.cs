18abb8cd899a3d20840721f65ff8846a4a0108fb
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2024-07-09 15:54:05 -0400

    Huge Bullet Attack Mostly Done

--- Assets/Scripts/_Boss/_Attacks/HugeBulletAttack.cs
+++ Assets/Scripts/_Boss/_Attacks/HugeBulletAttack.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class HugeBulletAttack : MonoBehaviour, IBossAttack
 {
+    private Transform bulletPrefab;
+    private float shootTimerMax = 5f;
+    private int shootDamage = 1;
+
+    private float shootTimer;
+    private bool isAttacking;
+
+    private void Awake()
+    {
+        bulletPrefab = Resources.Load<Transform>("HugeBullet");
+    }
+
+    private void Update()
+    {
+        if (!isAttacking) return;
+
+        shootTimer -= Time.deltaTime;
+
+        if (shootTimer < 0f)
+        {
+            Shoot();
+            shootTimer += shootTimerMax;
+        }
+    }
+
+    private void Shoot()
+    {
+        Transform bullet1 = ObjectPooler.Instance.InstantiateWithPool(bulletPrefab, new Vector3(-64f, 20f, 0f), Quaternion.identity);
+        bullet1.GetComponent<Bullet>().SetUp(Vector3.right, shootDamage, transform);
+
+        Transform bullet2 = ObjectPooler.Instance.InstantiateWithPool(bulletPrefab, new Vector3(64f, 0f, 0f), Quaternion.identity);
+        bullet2.GetComponent<Bullet>().SetUp(Vector3.left, shootDamage, transform);
+
+        Transform bullet3 = ObjectPooler.Instance.InstantiateWithPool(bulletPrefab, new Vector3(-64f, -20f, 0f), Quaternion.identity);
+        bullet3.GetComponent<Bullet>().SetUp(Vector3.right, shootDamage, transform);
+    }
+
     public void Attack()
     {
-
+        isAttacking = true;
+        transform.position = new Vector3(0f, 250f, 0f);
     }
 
     public void StopAttack()
     {
-
+        isAttacking = false;
+        transform.position = Vector3.zero;
     }
 }
