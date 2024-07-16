94b0faefed4b029d5f1b9459c9df13800b1c822d
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2024-07-16 15:58:16 -0400

    Bomb Attack Done (Balance Needed)

---
+++ Assets/Scripts/BombBullet.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombBullet : MonoBehaviour
+{
+    [SerializeField] private TrailRenderer trail;
+    [SerializeField] private Transform explosionParticles;
+    [SerializeField] private GameObject explosionIndicator;
+
+    private new Rigidbody2D rigidbody2D;
+
+    private Transform shooter;
+
+    private float decelerationAmount;
+    private int maxDamageAmount;
+    private float explosionRadius = 36f;
+    private Vector3 explosionPos;
+
+    private float explosionTimer;
+    private bool reachedPos;
+
+    public void SetUp(Vector3 explosionPos, int maxDamageAmount, Transform shooter)
+    {
+        trail.Clear();
+
+        rigidbody2D = GetComponent<Rigidbody2D>();
+
+        this.maxDamageAmount = maxDamageAmount;
+        this.shooter = shooter;
+        this.explosionPos = explosionPos;
+
+        Vector2 shootDir = (explosionPos - transform.position).normalized;
+
+        float initSpeed = Vector3.Distance(explosionPos, transform.position) * 3;
+        decelerationAmount = 2 * (initSpeed - Vector3.Distance(explosionPos, transform.position));
+
+        rigidbody2D.velocity = shootDir * initSpeed;
+
+        explosionTimer = 1f;
+
+        trail.widthMultiplier = transform.localScale.x;
+    }
+
+    private void FixedUpdate()
+    {
+        if (reachedPos) return;
+
+        rigidbody2D.velocity =
+            rigidbody2D.velocity.normalized *
+            (rigidbody2D.velocity.magnitude - decelerationAmount * Time.fixedDeltaTime);
+
+        if (Vector3.Distance(rigidbody2D.position, explosionPos) <= 0.25f)
+        {
+            rigidbody2D.velocity = Vector2.zero;
+            rigidbody2D.position = explosionPos;
+            reachedPos = true;
+            explosionIndicator.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if (shooter == null) ObjectPooler.Instance.DestoryWithPool(transform);
+
+        if (reachedPos) explosionTimer -= Time.deltaTime;
+
+        if (explosionTimer < 0f)
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        Transform particles = Instantiate(explosionParticles, transform.position, Quaternion.identity);
+        particles.localScale = Vector3.one * 3f;
+
+        Collider2D[] collider2DArray = Physics2D.OverlapCircleAll(transform.position, explosionRadius);
+
+        foreach (Collider2D collider2D in collider2DArray)
+        {
+            if (collider2D.TryGetComponent(out HealthSystem health))
+            {
+                int damageChangeInterval = Mathf.FloorToInt(explosionRadius / maxDamageAmount);
+
+                int damageAmount = maxDamageAmount - 
+                    Mathf.FloorToInt(Vector3.Distance(transform.position, collider2D.transform.position) / damageChangeInterval);
+
+                health.Damage(damageAmount);
+            }
+        }
+
+        ObjectPooler.Instance.DestoryWithPool(transform);
+    }
+
+    private void OnDisable()
+    {
+        reachedPos = false;
+        explosionIndicator.SetActive(false);
+    }
+}
--- Assets/Scripts/_Boss/_Attacks/BombAttack.cs
+++ Assets/Scripts/_Boss/_Attacks/BombAttack.cs
@@ -4,13 +4,53 @@ using UnityEngine;
 
 public class BombAttack : MonoBehaviour, IBossAttack
 {
+    private Transform bulletPrefab;
+    private float shootTimerMax = 2f;
+    private int explosionDamage = 6;
+
+    private float shootTimer;
+    private bool isAttacking;
+
+    private void Awake()
+    {
+        bulletPrefab = Resources.Load<Transform>("BossBomb");
+    }
+
+    private void Update()
+    {
+        if (!isAttacking) return;
+
+        shootTimer -= Time.deltaTime;
+
+        if (shootTimer < 0f)
+        {
+            Shoot();
+            shootTimer += shootTimerMax;
+        }
+    }
+
+    private void Shoot()
+    {
+        Vector2 maxBombPos = new Vector2(30, 20);
+        Vector2 minBombPos = new Vector2(-30, -20);
+
+        Vector2 bombExplosionPos = new Vector2(Random.Range(minBombPos.x, maxBombPos.x), Random.Range(minBombPos.y, maxBombPos.y));
+
+        Vector3 randomPosOutsideCamera = new Vector3(Random.Range(-30f*16/9, 30f*16/9), Random.Range(0, 1) == 0 ? 22.5f : -22.5f);
+
+        Transform bomb = ObjectPooler.Instance.InstantiateWithPool(bulletPrefab, randomPosOutsideCamera, Quaternion.identity);
+        bomb.GetComponent<BombBullet>().SetUp(bombExplosionPos, explosionDamage, transform);
+    }
+
     public void Attack()
     {
-        
+        isAttacking = true;
+        transform.position = new Vector3(0f, 250f, 0f);
     }
 
     public void StopAttack()
     {
-
+        isAttacking = false;
+        transform.position = Vector3.zero;
     }
 }
