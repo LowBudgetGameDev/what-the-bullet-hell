3b4322beb6748f1b1b92d17e8d7ee460576ee619
LowBudgetGameDev <148505424+LowBudgetGameDev@users.noreply.github.com>
2024-07-29 14:26:03 -0400

    Added Muffled Music when Game Paused

--- Assets/Scripts/_Managers/MusicManager.cs
+++ Assets/Scripts/_Managers/MusicManager.cs
@@ -8,10 +8,12 @@ public class MusicManager : MonoBehaviour
     [SerializeField] private AudioClip bossTheme;
 
     private AudioSource audioSource;
+    private AudioLowPassFilter lowPassFilter;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        lowPassFilter = GetComponent<AudioLowPassFilter>();
 
         audioSource.clip = mainTheme;
 
@@ -21,6 +23,24 @@ public class MusicManager : MonoBehaviour
     private void Start()
     {
         GameManager.Instance.OnBossBattleBegin += MusicManager_OnBossBattleBegin;
+        GameManager.Instance.OnMaxLevelReached += MusicManager_OnMaxLevelReached;
+    }
+
+    private void Update()
+    {
+        if (Time.timeScale == 0)
+        {
+            lowPassFilter.cutoffFrequency = 5000;
+        }
+        else
+        {
+            lowPassFilter.cutoffFrequency = 22000;
+        }
+    }
+
+    private void MusicManager_OnMaxLevelReached(object sender, System.EventArgs e)
+    {
+        audioSource.Stop();
     }
 
     private void MusicManager_OnBossBattleBegin(object sender, System.EventArgs e)
